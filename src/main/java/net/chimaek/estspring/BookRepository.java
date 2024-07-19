1617daa84f918e64375b5c94e36df9e3c355b0d0
seungju.kim <pipiru100@gmail.com>
2024-07-19 14:23:19 +0900

    init

--- src/main/java/net/chimaek/estspring/BookRepository.java
+++ src/main/java/net/chimaek/estspring/BookRepository.java
@@ -1,5 +1,21 @@
 package net.chimaek.estspring;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {}
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+  // ISBN으로 책 찾기
+  Optional<Book> findByIsbn(String isbn);
+
+  // 가격 범위로 책 목록 찾기
+  List<Book> findByPriceBetween(Double minPrice, Double maxPrice);
+
+  // 재고가 특정 수량 이상인 책 목록 찾기
+  List<Book> findByStockGreaterThanEqual(Integer minStock);
+
+  // 제목에 특정 키워드가 포함된 책 목록 찾기
+  List<Book> findByTitleContaining(String keyword);
+
+}
--- src/main/java/net/chimaek/estspring/BookService.java
+++ src/main/java/net/chimaek/estspring/BookService.java
@@ -1,6 +1,91 @@
 package net.chimaek.estspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class BookService {}
+@Transactional(readOnly = true)
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional
+    public BookDTO createBook(BookDTO bookDTO) {
+        Book book = bookDTO.toEntity();
+        Book savedBook = bookRepository.save(book);
+        return BookDTO.fromEntity(savedBook);
+    }
+
+    @Transactional(readOnly = true)
+    public List<BookDTO> getAllBooks() {
+
+        return bookRepository.findAll().stream()
+            .map(BookDTO::fromEntity)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<BookDTO> getBookById(Long id) {
+        return bookRepository.findById(id)
+            .map(BookDTO::fromEntity);
+    }
+
+    @Transactional
+    public Optional<BookDTO> updateBook(Long id, BookDTO bookDTO) {
+        return bookRepository.findById(id)
+            .map(existingBook -> {
+                existingBook.setTitle(bookDTO.getTitle());
+                existingBook.setAuthor(bookDTO.getAuthor());
+                existingBook.setIsbn(bookDTO.getIsbn());
+                existingBook.setPrice(bookDTO.getPrice());
+                existingBook.setStock(bookDTO.getStock());
+                return BookDTO.fromEntity(bookRepository.save(existingBook));
+            });
+    }
+
+    @Transactional
+    public boolean deleteBook(Long id) {
+        return bookRepository.findById(id)
+            .map(book -> {
+                bookRepository.delete(book);
+                return true;
+            })
+            .orElse(false);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<BookDTO> getBookByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+            .map(BookDTO::fromEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<BookDTO> getBooksByPriceRange(Double minPrice, Double maxPrice) {
+        return bookRepository.findByPriceBetween(minPrice, maxPrice).stream()
+            .map(BookDTO::fromEntity)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<BookDTO> getBooksWithStockGreaterThan(Integer minStock) {
+        return bookRepository.findByStockGreaterThanEqual(minStock).stream()
+            .map(BookDTO::fromEntity)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<BookDTO> getBooksByTitleContaining(String keyword) {
+        return bookRepository.findByTitleContaining(keyword).stream()
+            .map(BookDTO::fromEntity)
+            .collect(Collectors.toList());
+    }
+}
