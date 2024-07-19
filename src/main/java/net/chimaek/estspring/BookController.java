a5f1a0216584143ff2920e3d8060368f6f29bd4a
seungju.kim <pipiru100@gmail.com>
2024-07-19 11:33:48 +0900

    init

---
+++ src/main/java/net/chimaek/estspring/BookController.java
@@ -0,0 +1,16 @@
+package net.chimaek.estspring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+}
---
+++ src/main/java/net/chimaek/estspring/BookService.java
@@ -0,0 +1,6 @@
+package net.chimaek.estspring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {}
