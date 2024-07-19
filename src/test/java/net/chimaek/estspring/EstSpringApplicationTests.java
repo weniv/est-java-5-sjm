0c0ca309c9525775f3b5943892015f91896006dc
seungju.kim <pipiru100@gmail.com>
2024-07-19 10:31:32 +0900

    init

---
+++ src/test/java/net/chimaek/estspring/EstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chimaek.estspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
